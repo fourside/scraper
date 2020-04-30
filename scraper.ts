9026499a9fe15fdedae7b9c65e1c929f4c2bf06c
fourside <fourside@gmail.com>
2020-04-30 20:41:05 +0900

    bugfix: instanceof was broken for custom error
    https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work

--- scraper.ts
+++ scraper.ts
@@ -69,6 +69,7 @@ export class RetryError extends Error {
   constructor(private err: any, private nextUrl: string) {
     super(err);
     this.name = new.target.name;
+    Object.setPrototypeOf(this, RetryError.prototype);
   }
 
   getNextUrl() {
